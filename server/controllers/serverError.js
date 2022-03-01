3c994b0a1c9df8c34c68a9a04d3f3e8954d3472e
zaher-aa <zaherabuamro@gmail.com>
2022-03-01 17:18:33 +0200

    Edited client and server errors Relates #10

--- server/controllers/serverError.js
+++ server/controllers/serverError.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 
 const serverError = (req, res) => {
-  res.status(500).sendFile(join(__dirname, '../public/500.html'));
+  res.status(500).sendFile(join(__dirname, '../../public/assets/html/500.html'));
 };
 
 module.exports = serverError;
