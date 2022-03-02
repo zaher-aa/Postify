30ae67bd498ffe7abf06ad71757c1c5ddcc5092e
zaher-aa <zaherabuamro@gmail.com>
2022-03-02 06:58:49 +0200

    fetch users data from api Relates #26 and #29

---
+++ public/assets/js/fetch.js
@@ -0,0 +1,11 @@
+const getUser = (url) => fetch(url)
+  .then((res) => res.json());
+
+const addUser = (reqBody, reqMethod, url) => fetch(url, {
+  method: reqMethod,
+  body: JSON.stringify(reqBody),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then((res) => res.json());
