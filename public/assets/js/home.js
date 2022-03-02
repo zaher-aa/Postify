11fda7fd3f742f383ea5c461100febd351e06e61
zaher-aa <zaherabuamro@gmail.com>
2022-03-02 15:26:09 +0200

    Handled user profile front-end side Relates #5 and #7

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('.publicherInfo').forEach((user) => {
   user.addEventListener('click', (e) => {
-    const userId = e.currentTarget.parentElement.parentElement.dataset.userid;
-    window.location.href = `/users/${userId}`;
+    window.location.href = '/profile';
   });
 });
