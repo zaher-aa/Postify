03b8701d783a710886c6c7fbd77174da5d7832ec
zaher-aa <zaherabuamro@gmail.com>
2022-03-02 06:59:57 +0200

    Edited helmet and added serve-favicon module Relates #26 and #29

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
+const { join } = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const compression = require('compression');
 const helmet = require('helmet');
-const { join } = require('path');
-
 const router = require('./routes');
 
 const app = express();
 
+app.use(favicon(join(__dirname, '../public/favicon.ico')));
 app.use(compression());
 app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      'img-src': ["'self'", 'https: data:'],
+    },
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '../public')));
