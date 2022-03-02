b6f20a54a09bb01851e72b02a25bb01e1a9a9362
zaher-aa <zaherabuamro@gmail.com>
2022-03-02 07:05:15 +0200

    Created checkuser exists logic Relates #29

---
+++ server/controllers/checkUser.js
@@ -0,0 +1,19 @@
+const checkUserQuery = require('../database/queries/checkUserQuery');
+
+const checkUser = (req, res) => {
+  const { email, password } = req.query;
+
+  checkUserQuery(email, password)
+    .then((data) => {
+      if (data.rows.length) {
+        res.json({
+          username: data.rows[0].username, email: data.rows[0].email, id: data.rows[0].id,
+        });
+      } else {
+        res.json(data.rows);
+      }
+    })
+    .catch(() => res.status(500).json({ message: 'Internal Server Error' }));
+};
+
+module.exports = checkUser;
