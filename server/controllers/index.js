f312526272fd66f9a3cf2faf5ed464b7f3072f11
zaher-aa <zaherabuamro@gmail.com>
2022-03-02 07:06:18 +0200

    Added handlers imports and exports Relates #26 and #29

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,11 @@
+const checkUser = require('./checkUser');
+const addUser = require('./addUser');
 const clientError = require('./clientError');
 const serverError = require('./serverError');
 
 module.exports = {
+  checkUser,
+  addUser,
   clientError,
   serverError,
 };
